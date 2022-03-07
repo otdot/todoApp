a3cb90a73a6b8a01cb49a5c70bae9165ffc7d227
otdot <s2200126@BC-C02GR0BSQ6LR-MacBook-Air.local>
2022-03-07 14:28:18 +0200

    made x and check button functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#formtodo");
 const todolist = document.querySelector("#todolist");
 const reset = document.querySelector("#reset");
+const completedList = document.querySelector("#completedList");
 
 const addTodo = (e) => {
   e.preventDefault();
@@ -11,13 +12,23 @@ const addTodo = (e) => {
       `<li class="taskSection"> <p> ${input} </p> <i class="x fa-solid fa-xmark"></i> <i class="check fa-solid fa-check"></i> </li>`
     );
   }
-  tasks = [...document.querySelectorAll("#todolist li")];
+  xS = document.querySelectorAll(".x");
+  checkMarks = document.querySelectorAll(".check");
+  xS.forEach((task) => task.addEventListener("click", deleteTask));
+  checkMarks.forEach((check) => check.addEventListener("click", completeTask));
   document.querySelector("#addtodo").value = "";
-  tasks.forEach((task) => task.addEventListener("click", deleteTask));
 };
 
 const deleteTask = (e) => {
-  e.currentTarget.remove();
+  e.currentTarget.parentNode.remove();
+};
+
+const completeTask = (e) => {
+  completedList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="taskSection"> <p> ${e.currentTarget.parentNode.children[0].textContent} </p> <i class="x fa-solid fa-xmark"></i> </li>`
+  );
+  e.currentTarget.parentNode.remove();
 };
 
 form.addEventListener("submit", addTodo);
